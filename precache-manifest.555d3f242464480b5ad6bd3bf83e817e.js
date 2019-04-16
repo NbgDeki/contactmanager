6ab50e71690d4baaff80b1e5305bfa5b6cc9d0d4
nbgdeki <radic.dejan.nbg@gmail.com>
2019-04-16 14:08:42 +0200

    Updates

---
+++ precache-manifest.555d3f242464480b5ad6bd3bf83e817e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cc26d9182ab8f34bf43",
+    "url": "/contactmanager/static/css/main.f4b39c60.chunk.css"
+  },
+  {
+    "revision": "8cc26d9182ab8f34bf43",
+    "url": "/contactmanager/static/js/main.aeea5470.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "a9eaa14e448dfd3ee808",
+    "url": "/contactmanager/static/css/2.3136f980.chunk.css"
+  },
+  {
+    "revision": "a9eaa14e448dfd3ee808",
+    "url": "/contactmanager/static/js/2.d5ed20e9.chunk.js"
+  },
+  {
+    "revision": "908e208193606bcbd4b0294bea30a7e3",
+    "url": "/contactmanager/index.html"
+  }
+];
